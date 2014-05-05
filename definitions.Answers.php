7666b9caad2d8b9c4a6be666168e713afd0b56e0
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:42:35 +0200

    Create definitions.Answers.php

---
+++ definitions.Answers.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Answers plugin.
+
+$definitions["Answer"] = "Одговор";
+$definitions["Answered"] = "Одговорено";
+$definitions["This post answered my question"] = "Овај коментар је одговорио на моје питање.";
+$definitions["Answered by %s"] = "Одговорено од %s";
+$definitions["See post in context"] = "Види коментар";
+$definitions["Remove answer"] = "Уклони одговор";
