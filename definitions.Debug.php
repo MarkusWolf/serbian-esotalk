a18f1e8e9262bdfd623d1d8637a36711e94e785b
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:47:08 +0200

    Create definitions.Debug.php

---
+++ definitions.Debug.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Debug plugin.
+
+$definitions["MySQL queries"] = "MySQL упита";
+$definitions["Page loaded in %s seconds"] = "Страница учитана за %s секунди";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES информације";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE информације";
