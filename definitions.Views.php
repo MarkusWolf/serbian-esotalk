e9ea1de8e3f12ef6e9333f950ef68fa56ab91c5a
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:57:17 +0200

    Create definitions.Views.php

---
+++ definitions.Views.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Profiles plugin.
+
+$definitions["gambit.order by views"] = "поређај по прегледу";
+$definitions["%s view"] = "%s преглед"
+$definitions["%s views"] = "%s прегледа";
