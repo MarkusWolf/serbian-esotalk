a02379248f57a69fded4fd8539bbde3bff272f7f
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:45:33 +0200

    Create definitions.BBCode.php

---
+++ definitions.BBCode.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+//Serbian Definitions for the BBCode plugin.
+
+$definitions["Code"] = "Код";
+$definitions["Image"] = "Слика";
+$definitions["Link"] = "Линк";
+$definitions["Strike"] = "Прецртано";
+$definitions["Header"] = "Наслов";
+$definitions["Italic"] = "Искошено";
+$definitions["Bold"] = "Подебљано";
