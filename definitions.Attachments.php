81239d271df6fe99b59f553619864aac074f2214
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:44:09 +0200

    Create definitions.Attachments.php

---
+++ definitions.Attachments.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Answers plugin.
+
+$definitions["Attach a file"] = "Прикачи фајл";
+$definitions["Drop files to upload"] = "Пренеси фајл за додавање";
+$definitions["message.attachmentNotFound"] = "Из неког разлога фајл не може бити приказан. Можда не постоји, или немате дозволу да га видите.";
