ab1a9e7ecde54e08b2cd88415f8236f6ef2fe893
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:48:38 +0200

    Create definitions.Likes.php

---
+++ definitions.Likes.php
@@ -0,0 +1,11 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Profiles plugin.
+
+$definitions["Unlike"] = "Не волим";
+$definitions["Like"] = "Волим";
+$definitions["Members Who Liked This Post"] = "Чланови који воле овај коментар";
+$definitions["%s likes this."] = "%s воли ово.";
+$definitions["%s like this."] = "%s воли ово.";
