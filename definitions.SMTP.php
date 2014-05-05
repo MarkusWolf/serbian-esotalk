997b2dcfc1ec1f4a019587081582ff44a463a351
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:55:41 +0200

    Create definitions.SMTP.php

---
+++ definitions.SMTP.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the SMTP plugin.
+
+$definitions["Server"] = "Сервер";
+$definitions["Port"] = "Порт";
+$definitions["Authentication"] = "Аутентикација";
+$definitions["Normal"] = "Нормални мод";
+$definitions["TLS"] = "TLS";
+$definitions["SSL"] = "SSL";
