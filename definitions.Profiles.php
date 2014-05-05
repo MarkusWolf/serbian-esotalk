f871a52278ddf2bb247785f307d897d039283d21
MarkusWolf <vukwap@gmail.com>
2014-05-05 08:54:48 +0200

    Create definitions.Profiles.php

---
+++ definitions.Profiles.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Serbian Definitions for the Profiles plugin.
+
+$definitions["About"] = "Информације";
+$definitions["Location"] = "Локација";
+$definitions["Write something about yourself."] = "Напиши нешто о себи.";
